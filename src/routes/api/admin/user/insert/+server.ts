38a731f53bf1a44f84da283663c09ac1ca66eb37
Mark Gersaniva <mark.gersaniva@xtendops.us>
2024-01-15 18:21:23 +0800

    refactor: added user components

--- src/routes/api/admin/user/insert/+server.ts
+++ src/routes/api/admin/user/insert/+server.ts
@@ -43,6 +43,7 @@ export async function POST({ request, locals }: any) {
 		return new Response(
 			JSON.stringify({
 				status: 'Success',
+				message: 'Data inserted successfully',
 				response
 			})
 		);
--- src/routes/dashboard/users/+page.svelte
+++ src/routes/dashboard/users/+page.svelte
@@ -1,35 +1,21 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { SHA256 } from 'crypto-js';
+	import Create from '$lib/components/forms/user/Create.svelte';
 	import {
-		Autocomplete,
 		Drawer,
-		focusTrap,
 		getDrawerStore,
-		getToastStore,
 		Paginator,
 		Table,
 		tableMapperValues
 	} from '@skeletonlabs/skeleton';
 	import type {
-		AutocompleteOption,
 		DrawerSettings,
 		PaginationSettings,
 		TableSource,
-		ToastSettings
 	} from '@skeletonlabs/skeleton';
+	import { goto } from '$app/navigation';
 
-	let lastName: string,
-		firstName: string,
-		username: string,
-		password: string,
-		confirmPassword: string,
-		role: string = 'Encoder',
-		email: string,
-		phone: string;
 	let keyword: string = '';
-	let isFocused: boolean = true;
-
 	let sourceData: any = [];
 	let table: TableSource = {
 		// A list of heading labels.
@@ -56,8 +42,9 @@
 		}
 	}
 
-	const drawerSettings: DrawerSettings = {
-		id: 'createCustomer',
+	// drawer settings
+	const drawerCreate: DrawerSettings = {
+		id: 'createUser',
 		// Provide your property overrides:
 		bgDrawer: 'bg-gradient-to-t from-slate-900 via-gray-950 to-zinc-950 text-white',
 		bgBackdrop: 'bg-gradient-to-tr from-slate-900/50 via-gray-950/50 to-zinc-950/50',
@@ -69,12 +56,6 @@
 
 	const drawerStore = getDrawerStore();
 	drawerStore.close();
-
-	const toastStore = getToastStore();
-	const toastSettings: ToastSettings = {
-		message: '',
-		timeout: 5000
-	};
 
 	const filterTable = (keyword: string) => {
 		paginationSettings.page = 0;
@@ -102,8 +83,14 @@
 			paginationSettings.page * paginationSettings.limit,
 			paginationSettings.page * paginationSettings.limit + paginationSettings.limit
 		);
-		table.body = tableMapperValues(paginatedData, ['fullName', 'role', 'username', 'email', 'phone']);
-		table.meta = tableMapperValues(paginatedData, ['fullName', 'role', 'username', 'email', 'phone']);
+		table.body = tableMapperValues(paginatedData, [
+			'fullName',
+			'role',
+			'username',
+			'email',
+			'phone'
+		]);
+		table.meta = tableMapperValues(paginatedData, ['_id', 'role', 'username', 'email', 'phone']);
 		table.foot = ['Total', '', '', `<code class="code">${sourceData.length}</code>`];
 	};
 
@@ -130,6 +117,12 @@
 		await loadData();
 	});
 
+	// table row select handler
+	function tableSelectHandler(e: CustomEvent): void {
+		console.log(e.detail[0]);
+		goto(`/dashboard/users/${e.detail[0]}`);
+	}
+
 	$: filterTable(keyword);
 </script>
 
@@ -138,7 +131,7 @@
 		<h1 class="h3">Users</h1>
 	</header>
 	<section class="flex p-4 w-full gap-4">
-		<button class="btn variant-filled-primary" on:click={() => drawerStore.open(drawerSettings)}
+		<button class="btn variant-filled-primary" on:click={() => drawerStore.open(drawerCreate)}
 			>Add User</button
 		>
 		<input class="input ml-auto" type="text" placeholder="Search" bind:value={keyword} />
@@ -146,7 +139,7 @@
 </div>
 
 {#key sourceData}
-	<Table source={table} />
+	<Table source={table} interactive={true} on:selected={tableSelectHandler} />
 	<Paginator
 		class="mt-4"
 		bind:settings={paginationSettings}
@@ -157,143 +150,7 @@
 	/>
 {/key}
 <Drawer>
-	<form
-		method="POST"
-		autocomplete="off"
-		class="p-6"
-		use:focusTrap={isFocused}
-		on:submit|preventDefault={async () => {
-			try {
-				if (password != confirmPassword) {
-					toastSettings.message = 'Password and Confirm Password does not match';
-					toastSettings.background = 'variant-filled-error';
-					toastStore.trigger(toastSettings);
-					return;
-				}
-				const hashedPassword = await SHA256(password).toString();
-				let response = await fetch('/api/admin/user/insert', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify({
-						username: username,
-						lastName: lastName,
-						firstName: firstName,
-						password: hashedPassword,
-						email: email,
-						phone: phone,
-						role: role
-					})
-				});
-
-				let result = await response.json();
-
-				toastSettings.message = result.message;
-				toastStore.trigger(toastSettings);
-				loadData();
-				drawerStore.close();
-			} catch (error) {
-				toastSettings.message = error.message;
-				toastSettings.background = 'variant-filled-error';
-				toastStore.trigger(toastSettings);
-				console.error(error);
-			}
-		}}
-	>
-		<h2 class="h4">Create User</h2>
-		<label class="label mt-4">
-			<span>Email</span>
-			<input
-				class="input"
-				type="email"
-				placeholder="juandelacruz@sample.com"
-				name="email"
-				bind:value={email}
-				required
-			/>
-		</label>
-		<label class="label mt-4">
-			<span>Username</span>
-			<input
-				class="input"
-				type="text"
-				placeholder="juan.delacruz"
-				name="username"
-				bind:value={username}
-				required
-			/>
-		</label>
-		<label class="label mt-4">
-			<span>Password</span>
-			<input
-				class="input"
-				type="password"
-				placeholder="must be between 8~16 characters long"
-				name="password"
-				bind:value={password}
-				required
-			/>
-		</label>
-		<label class="label mt-4">
-			<span>Confirm Password</span>
-			<input
-				class="input"
-				type="password"
-				placeholder="confirm password"
-				name="confirmPassword"
-				bind:value={confirmPassword}
-				required
-			/>
-		</label>
-		<label class="label mt-4">
-			<span>Role</span>
-			<select class="select" bind:value={role} required>
-				<option value="Encoder">Encoder</option>
-				<option value="Manager">Manager</option>
-				<option value="Administrator">Admin</option>
-			</select>
-		</label>
-		<hr class="mt-4" />
-		<label class="label mt-4">
-			<span>Last Name</span>
-			<input
-				class="input"
-				type="text"
-				placeholder="Dela Cruz"
-				name="lastName"
-				bind:value={lastName}
-				required
-			/>
-		</label>
-		<label class="label mt-4">
-			<span>First Name</span>
-			<input
-				class="input"
-				type="text"
-				placeholder="Juan"
-				name="firstName"
-				bind:value={firstName}
-				required
-			/>
-		</label>
-		<label class="label mt-4">
-			<span>Phone</span>
-			<input
-				class="input"
-				type="text"
-				placeholder="+639171234567"
-				name="phone"
-				bind:value={phone}
-				required
-			/>
-		</label>
-
-		<div class="flex gap-4 place-content-end w-full">
-			<button type="submit" class="btn variant-filled-success mt-4">Submit</button>
-			<button type="button" class="btn variant-filled mt-4" on:click={() => drawerStore.close()}
-				>Cancel</button
-			>
-		</div>
-	</form>
+	{#if $drawerStore.id === 'createUser'}
+		<Create {drawerStore} {loadData} />
+	{/if}
 </Drawer>
